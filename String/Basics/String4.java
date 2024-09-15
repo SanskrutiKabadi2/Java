3279089ffde5b79ce9f59a0f016b9710bf0c3cf3
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-09-15 19:34:12 +0530

    String

---
+++ String/Basics/String4.java
@@ -0,0 +1,30 @@
+class String4{
+	public static void main(String[] args){
+		String str1="Sanskruti";
+		String str2="Kabadi";
+		String str3="Sanskruti Kabadi";
+		String str4=str1+str2;
+		System.out.println(System.identityHashCode(str1));
+		System.out.println(System.identityHashCode(str2));
+		System.out.println(System.identityHashCode(str3));
+		System.out.println(System.identityHashCode(str4));
+	
+	
+	
+	
+	
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
