30203faf1d4b83a248e968b3e08fabcf5bb81afc
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-09-15 19:30:03 +0530

    String1

---
+++ String/String1.java
@@ -0,0 +1,11 @@
+class String1{
+	public static void main(String[] args){
+		String str1="Sanskruti";
+		String str2="Sanskruti";
+		System.out.println(System.identityHashCode(str1));
+		System.out.println(System.identityHashCode(str2));
+	
+	}
+
+
+}
