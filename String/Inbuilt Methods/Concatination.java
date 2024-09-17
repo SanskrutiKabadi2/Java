e1f7e6fd884ddec2aae8a4afc82cc2d09410d514
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-09-17 10:55:00 +0530

    Concatination

---
+++ String/Inbuilt Methods/Concatination.java
@@ -0,0 +1,15 @@
+class ConcatPrac{
+	public static void main(String[] args){
+		String str1="Hello";
+		String str2="world";
+		//String str3=str1.concat(str2);
+		System.out.println(str1.concat(str2));
+	
+	
+	}
+
+}
+
+//in this code the thing that we have to keep in mind is we can write the method inside system.out.printl which written something
+//concat(String str)
+//return ->String
