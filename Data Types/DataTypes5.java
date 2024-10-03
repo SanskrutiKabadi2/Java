d285c10088f9742eb0b376481593bf82c1492534
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-10-03 22:29:57 +0530

    Boolean

---
+++ Data Types/DataTypes5.java
@@ -0,0 +1,10 @@
+class DataTypes5{
+        public static void main(String[] args){
+                boolean answer1=true,answer2=false;
+
+                System.out.println("Answer1= "+answer1);
+		System.out.println("Answer2= "+answer2);
+        }
+
+
+}
