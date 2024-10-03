8c0ef385548d7f55069ab30351aaf186d041d4dd
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-10-03 22:33:48 +0530

    Data Types

---
+++ Data Types/DataTypes9.java
@@ -0,0 +1,23 @@
+class DataTypes9{
+        public static void main(String[] args){
+		boolean bool=true;
+                byte b=46;
+		short s=89;
+		int i=4557;
+		float f=56.99f;
+		long l=446565;
+		double d=56.676453433435467;
+		char c='A';
+		System.out.println("bool = "+bool);
+                System.out.println("b = "+b);
+		System.out.println("s = "+s);
+		System.out.println("i = "+i);
+		System.out.println("f = "+f);
+		System.out.println("l = "+l);
+		System.out.println("d = "+d);
+		System.out.println("c = "+c);
+
+        }
+
+
+}
