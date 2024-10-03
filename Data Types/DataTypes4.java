8da330548fd02b7e5696950a4fada23101fe55d4
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-10-03 22:29:10 +0530

    Double

---
+++ Data Types/DataTypes4.java
@@ -0,0 +1,9 @@
+class DataTypes4{
+        public static void main(String[] args){
+                double distance=20.2397477785784;
+                
+                System.out.println("Distance = "+distance);
+        }
+
+
+}
