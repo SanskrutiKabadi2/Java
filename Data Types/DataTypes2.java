624f016c13b72833d7b0ab19b1b5ce6675865398
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-10-03 22:28:16 +0530

    Float

---
+++ Data Types/DataTypes2.java
@@ -0,0 +1,9 @@
+class DataTypes2{
+        public static void main(String[] args){
+                
+                float salary=2023974.7f;
+                System.out.println("Salary = "+salary);
+        }
+
+
+}
