e9485bc57cf0aa4be9db152048b962de19214e68
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-10-03 22:28:40 +0530

    Long

---
+++ Data Types/DataTypes3.java
@@ -0,0 +1,9 @@
+class DataTypes3{
+        public static void main(String[] args){
+                
+                long distance=2023974774;
+                System.out.println("Distance = "+distance);
+        }
+
+
+}
