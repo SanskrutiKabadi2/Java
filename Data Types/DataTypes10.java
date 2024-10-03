7b6e149a5a158f6d94fc8f4f6b34b5dd783ca24d
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-10-03 22:34:14 +0530

    Data Types

---
+++ Data Types/DataTypes10.java
@@ -0,0 +1,12 @@
+class DataTypes10{
+        public static void main(String[] args){
+                int a=678909;//4 bytes
+		
+		float b=a; //4 bytes
+                System.out.println("a= "+a);
+		System.out.println("b= "+b);
+
+        }
+
+
+}
