14b127859762ee97e8fc1e76a660c70767f9cfdb
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-10-04 17:00:55 +0530

    Division

---
+++ Operators/Arithmetic/Operators4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Operators4{
+        public static void main(String[] args){
+                Scanner Sc=new Scanner(System.in);
+                int A,B;
+		float Div;
+                System.out.println("enter first number :");
+                A=Sc.nextInt();
+                System.out.println("enter second number :");
+                B=Sc.nextInt();
+                Div=A/B;
+                System.out.println("Div = "+Div);
+
+        }
+
+}
