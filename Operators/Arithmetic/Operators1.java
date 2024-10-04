d43f63b6116b8f16ab9175aca1d70c950afc7853
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-10-04 16:58:09 +0530

    Addition

---
+++ Operators/Arithmetic/Operators1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Operators1{
+	public static void main(String[] args){
+		Scanner Sc=new Scanner(System.in);
+		int A,B,Sum;
+		System.out.println("enter first number :");
+		A=Sc.nextInt();
+		System.out.println("enter second number :");
+                B=Sc.nextInt();
+		Sum=A+B;
+		System.out.println("Sum = "+Sum);
+	
+	}
+
+}
