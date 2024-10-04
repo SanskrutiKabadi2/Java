1ca9a1c9737ce9c844e203a9e713e1d3ae76efb1
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-10-04 17:03:27 +0530

    Modulus

---
+++ Operators/Arithmetic/Operators5.java
@@ -0,0 +1,11 @@
+class Operators5{
+        public static void main(String[] args){
+               
+                float A=150;
+                float B=20;
+                System.out.println("A%B = "+A%B);
+		
+
+        }
+
+}
