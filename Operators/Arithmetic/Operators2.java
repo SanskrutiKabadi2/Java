effd723717d331886818a45ffa552cf9da3b6224
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-10-04 16:58:50 +0530

    Substraction

---
+++ Operators/Arithmetic/Operators2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Operators2{
+        public static void main(String[] args){
+                Scanner Sc=new Scanner(System.in);
+                int A,B,Sub;
+                System.out.println("enter first number :");
+                A=Sc.nextInt();
+                System.out.println("enter second number :");
+                B=Sc.nextInt();
+                Sub=A-B;
+                System.out.println("Sub = "+Sub);
+
+        }
+
+}
