dc1e47f6d0deff090531c40d581a210230e9ca52
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-10-04 17:00:29 +0530

    Multiplication

---
+++ Operators/Arithmetic/Operators3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Operators3{
+        public static void main(String[] args){
+                Scanner Sc=new Scanner(System.in);
+                int A,B,Mul;
+                System.out.println("enter first number :");
+                A=Sc.nextInt();
+                System.out.println("enter second number :");
+                B=Sc.nextInt();
+                Mul=A*B;
+                System.out.println("Mul = "+Mul);
+
+        }
+
+}
