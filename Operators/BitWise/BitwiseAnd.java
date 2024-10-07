f83b8f500b6edb642ead39cc4550d366976bd7a5
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-10-07 14:19:09 +0530

    BitwiseAnd

---
+++ Operators/BitWise/BitwiseAnd.java
@@ -0,0 +1,19 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Operators29{
+        public static void main(String[] args){
+                Scanner Sc=new Scanner(System.in);
+                int A,B;
+                System.out.println("enter first number :");
+                A=Sc.nextInt();
+                System.out.println("enter second number :");
+                B=Sc.nextInt();
+                int C=A|B;
+		System.out.println( "A|B = "+C);
+               
+
+
+        }
+
+}
