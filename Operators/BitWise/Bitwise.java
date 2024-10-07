740283473878c55a1373dccf7324d2b216fcc417
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-10-07 14:21:29 +0530

    Bitwise

---
+++ Operators/BitWise/Bitwise.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Operators30{
+        public static void main(String[] args){
+                Scanner Sc=new Scanner(System.in);
+                int A,B;
+                System.out.println("enter first number :");
+                A=Sc.nextInt();
+                System.out.println("enter second number :");
+                B=Sc.nextInt();
+                System.out.println( A & B);
+                System.out.println( A | B);
+                System.out.println(A ^ B);
+		System.out.println(~A);
+		System.out.println(~B);
+
+
+        }
+
+}
