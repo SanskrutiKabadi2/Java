ef22b73eae890f42028f3aae5974e09a74a9f818
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-10-07 14:50:01 +0530

    RightShift

---
+++ Operators/BitWise/RightShift.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Operators28{
+        public static void main(String[] args){
+                Scanner Sc=new Scanner(System.in);
+                int A,B;
+                System.out.println("enter first number :");
+                A=Sc.nextInt();
+                System.out.println("enter second number :");
+                B=Sc.nextInt();
+                System.out.println( A>>2);
+                System.out.println(B>>2);
+
+
+        }
+
+}
