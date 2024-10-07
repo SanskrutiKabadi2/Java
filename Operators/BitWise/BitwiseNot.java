b334349c41424d23b8c7162e2f843746fa66de08
Sanskruti kabadi <sanskrutikabadi2@gmail.com>
2024-10-07 14:21:00 +0530

    BitwiseNot

---
+++ Operators/BitWise/BitwiseNot.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Operators29{
+        public static void main(String[] args){
+                Scanner Sc=new Scanner(System.in);
+                int A,B;
+                System.out.println("enter first number :");
+                A=Sc.nextInt();
+                System.out.println( "~A = "+ ~A);
+                
+		
+               
+
+
+        }
+
+}
